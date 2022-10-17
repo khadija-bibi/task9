da6e31d5675ff646e63a2c5bd2a52a3779ae736f
khadija-bibi <sub99way1122@gmail.com>
2022-10-17 17:23:51 +0500

    task 9

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(home: Home()));
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+
+
+
+    );
+  }
+}
+
+
